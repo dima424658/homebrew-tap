eda1cd54535e5bac4915f8bc67d8cc7e2df7a027
Dmitry Pankov <dmitry@enula.ru>
2024-11-25 21:33:05 +0300

    Update tarantool to 2.11.5

--- Formula/tarantool@2.11.rb
+++ Formula/tarantool@2.11.rb
@@ -1,18 +1,20 @@
 class TarantoolAT211 < Formula
   desc "In-memory database and Lua application server"
   homepage "https://tarantool.org/"
-  url "https://download.tarantool.org/tarantool/src/tarantool-2.11.4.tar.gz"
-  sha256 "454bb48a7b3c66f353c1005728f1f19247b0b470991bf2347b8986fce2a7bc9f"
+  url "https://download.tarantool.org/tarantool/src/tarantool-2.11.5.tar.gz"
+  sha256 "a09a26b04f97c4e211db196211ecf9865ad8e1b98850ec1a136ba1a7f1b057c9"
   license "BSD-2-Clause"
   version_scheme 1
-  head "https://github.com/tarantool/tarantool.git", branch: "2.11.4"
+  head "https://github.com/tarantool/tarantool.git", branch: "master"
 
   depends_on "cmake" => :build
-  depends_on "icu4c"
+  depends_on "curl"
+  depends_on "icu4c@76"
+  depends_on "libyaml"
   depends_on "openssl@3"
   depends_on "readline"
+  depends_on "zstd"
 
-  uses_from_macos "curl"
   uses_from_macos "ncurses"
 
   on_linux do
@@ -25,36 +27,30 @@ class TarantoolAT211 < Formula
               "#define COMPILER_INFO \"@CMAKE_C_COMPILER_ID@-@CMAKE_C_COMPILER_VERSION@\"",
               "#define COMPILER_INFO \"/usr/bin/clang /usr/bin/clang++\""
 
+    # Workaround for clang >= 16 until upstream fix is available[^1].
+    # Also, trying to apply LuaJIT commit[^2] worked on Xcode 16 but caused issue on Xcode 15.
+    #
+    # [^1]: https://github.com/tarantool/tarantool/issues/10566
+    # [^2]: https://github.com/LuaJIT/LuaJIT/commit/2240d84464cc3dcb22fd976f1db162b36b5b52d5
+    ENV.append "LDFLAGS", "-Wl,-no_deduplicate" if DevelopmentTools.clang_build_version >= 1600
+
+    icu4c = deps.find { |dep| dep.name.match?(/^icu4c(@\d+)?$/) }
+                .to_formula
     args = std_cmake_args
     args << "-DCMAKE_INSTALL_MANDIR=#{doc}"
     args << "-DCMAKE_INSTALL_SYSCONFDIR=#{etc}"
     args << "-DCMAKE_INSTALL_LOCALSTATEDIR=#{var}"
     args << "-DENABLE_DIST=ON"
+    args << "-DCURL_ROOT=#{Formula["curl"].opt_prefix}"
+    args << "-DCURL_ROOT_DIR=#{Formula["curl"].opt_prefix}"
+    args << "-DICU_ROOT=#{icu4c.opt_prefix}"
     args << "-DOPENSSL_ROOT_DIR=#{Formula["openssl@3"].opt_prefix}"
     args << "-DREADLINE_ROOT=#{Formula["readline"].opt_prefix}"
     args << "-DENABLE_BUNDLED_LIBCURL=OFF"
-    args << "-DENABLE_BUNDLED_LIBYAML=ON"
-    args << "-DENABLE_BUNDLED_ZSTD=ON"
-
-    if OS.mac?
-      if MacOS.version >= :big_sur
-        sdk = MacOS.sdk_path_if_needed
-        lib_suffix = "tbd"
-      else
-        sdk = ""
-        lib_suffix = "dylib"
-      end
-
-      args << "-DCURL_INCLUDE_DIR=#{sdk}/usr/include"
-      args << "-DCURL_LIBRARY=#{sdk}/usr/lib/libcurl.#{lib_suffix}"
-      args << "-DCURSES_NEED_NCURSES=ON"
-      args << "-DCURSES_NCURSES_INCLUDE_PATH=#{sdk}/usr/include"
-      args << "-DCURSES_NCURSES_LIBRARY=#{sdk}/usr/lib/libncurses.#{lib_suffix}"
-      args << "-DICONV_INCLUDE_DIR=#{sdk}/usr/include"
-    else
-      args << "-DENABLE_BUNDLED_LIBUNWIND=ON"
-      args << "-DCURL_ROOT=#{Formula["curl"].opt_prefix}"
-    end
+    args << "-DENABLE_BUNDLED_LIBUNWIND=OFF"
+    args << "-DENABLE_BUNDLED_LIBYAML=OFF"
+    args << "-DENABLE_BUNDLED_ZSTD=OFF"
+    args << "-DLUAJIT_NO_UNWIND=ON"
 
     system "cmake", ".", *args
     system "make"
